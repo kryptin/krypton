7457152ef4d86e2536ebd6e21178aee38a96617c
DESKTOP-I10C14Q\chinagorom <jonas.asogwa@ikooba.com>
2018-04-16 14:23:18 +0100

    commit

--- graphql/mutations/group/index.js
+++
@@ -1,9 +0,0 @@
-import addGroup from './add';
-import removeAllGroups from './remove-all';
-import removeGroup from './remove';
-
-export default {
-    addGroup,
-    removeGroup,
-    removeAllGroups
-};
