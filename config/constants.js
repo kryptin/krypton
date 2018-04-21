a05b7f3b4e3371f8cf91e68b2e845820cd32984a
Ayorinde Kudoro <kudoroayorinde@gmail.com>
2018-04-21 17:40:53 +0100

    back to the correct DB_URL

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,6 @@
 export default {
     //PORT: process.env.PORT || 4000,
-    //DB_URL: 'mongodb://localhost/krypton_db',
-    DB_URL: 'mongodb://localhost/graphql',
+    DB_URL: 'mongodb://localhost/krypton_db',
     GRAPHQL_PATH: '/graphql',
     JWT_SECRET: 'sjhf887sdfhjsd8778'
 }
