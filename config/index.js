186c79978184849bfaea572745b9823dbc2743be
brainiacten <brainiacten@gmail.com>
2018-04-21 11:46:44 +0100

    Ready to serve GraphQL

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 import SERVER_ENV from './server.config';
 import LOG_ENV from './logger.config';
 
-var environment = process.env.NODE_ENV || 'production';
+var environment = process.env.NODE_ENV || 'development';
 
 var serverConf = SERVER_ENV[environment];
 var logConf = LOG_ENV[environment];
