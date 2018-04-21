185be67a47164c132923e8955c9147b920e78308
Ayorinde Kudoro <kudoroayorinde@gmail.com>
2018-04-21 17:39:06 +0100

    now getting the whole user object in the context
    Instead of just the id

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,7 @@
 export default {
     //PORT: process.env.PORT || 4000,
-    DB_URL: 'mongodb://localhost/krypton_db',
+    //DB_URL: 'mongodb://localhost/krypton_db',
+    DB_URL: 'mongodb://localhost/graphql',
     GRAPHQL_PATH: '/graphql',
     JWT_SECRET: 'sjhf887sdfhjsd8778'
 }
