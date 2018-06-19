aba5c77d843aedca28dcc774044513be1fdddbc5
Ayorinde <kudoroayorinde@gmail.com>
2018-06-19 08:52:27 +0100

    local changes

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,9 @@
 export default {
-    //PORT: process.env.PORT || 4000,
-    // development
-    // DB_URL: 'mongodb://localhost/krypton_db',
-    //production
-    DB_URL: process.env.MONGODB_URI || 'mongodb://krypton:buldozed@ds131329.mlab.com:31329/heroku_1kwwffk8',
-    GRAPHQL_PATH: '/graphql',
-    JWT_SECRET: 'sjhf887sdfhjsd8778'
-}
+  PORT: process.env.PORT || 4000,
+  // development
+  DB_URL: "mongodb://localhost/krypton_db",
+  //production
+  //DB_URL: process.env.MONGODB_URI || 'mongodb://krypton:buldozed@ds131329.mlab.com:31329/heroku_1kwwffk8',
+  GRAPHQL_PATH: "/graphql",
+  JWT_SECRET: "sjhf887sdfhjsd8778"
+};
--- config/server.config.js
+++ config/server.config.js
@@ -1,14 +1,14 @@
 const SERVER_ENV = {
-    'production': {
-        // 'SERVER_PORT': process.env.IWIP_SERVER_PORT || 4800
-        'SERVER_PORT': process.env.PORT || 4800
-    },
-    'preproduction': {
-        'SERVER_PORT': 4800
-    },
-    'development': {
-        'SERVER_PORT': 4800
-    }
+  production: {
+    SERVER_PORT: process.env.IWIP_SERVER_PORT || 4800
+    //SERVER_PORT: process.env.PORT || 4800
+  },
+  preproduction: {
+    SERVER_PORT: 4800
+  },
+  development: {
+    SERVER_PORT: 4800
+  }
 };
 
 export default SERVER_ENV;
